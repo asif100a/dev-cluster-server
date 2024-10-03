7abe6b0181956581371264c2056431658fc75d09
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-10-03 21:49:04 +0600

    Student delete functionality - completed

--- index.js
+++ index.js
@@ -102,6 +102,16 @@ async function run() {
       res.send(result);
     });
 
+    // Delete the Student data
+    app.delete('/deleteStudents/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id);
+
+      const filter = {_id: new ObjectId(id)};
+      const result = await studentCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
